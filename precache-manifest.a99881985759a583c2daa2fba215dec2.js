c7734303e0b62d9d74a8599c844540b57e65e104
claremburu <mburuclare8@gmail.com>
2021-11-28 08:49:00 +0300

    Updates

---
+++ precache-manifest.a99881985759a583c2daa2fba215dec2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e61ff2ca1bac98b44ce07cf3d5394d69",
+    "url": "/claremburu/CRUD-App/index.html"
+  },
+  {
+    "revision": "a573153d1c9517b8b27c",
+    "url": "/claremburu/CRUD-App/static/css/main.8a15d8e6.chunk.css"
+  },
+  {
+    "revision": "3cdef57c09bc45decc30",
+    "url": "/claremburu/CRUD-App/static/js/2.7e6d3f38.chunk.js"
+  },
+  {
+    "revision": "a573153d1c9517b8b27c",
+    "url": "/claremburu/CRUD-App/static/js/main.2a462364.chunk.js"
+  },
+  {
+    "revision": "7cd90f17e62671a7dae6",
+    "url": "/claremburu/CRUD-App/static/js/runtime~main.ef9102d4.js"
+  }
+]);
